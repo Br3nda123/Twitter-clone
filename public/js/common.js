5603020b6ab565739c22847c0c068449f2771569
Brandon Kunicki <brandon.kunicki@gmail.com>
2021-04-12 21:01:54 +0200

    Created timestamp function to get prettier information when post was publicated

--- public/js/common.js
+++ public/js/common.js
@@ -64,48 +64,76 @@ submitButton.addEventListener("click", (e) => {
 
 function createPostHtml(postData) {
 	const postedBy = postData.postedBy;
-	console.log(postedBy);
+
+	if (postedBy._id === undefined) {
+		return console.log("User object not populated");
+	}
+
 	const displayName = postedBy.firstName + " " + postedBy.lastName;
-	const timestamp = postData.createdAt;
+	const timestamp = timeDifference(new Date(), new Date(postData.createdAt));
 
 	const div = document.createElement("div");
 	div.className = "post";
 	div.innerHTML = `
-    <div class='mainContentContainer'>
-      <div class='userImageContainer'>
-        <img src='${postedBy.profilePic}'>
-      </div>
-      <div class='postContentContainer'>
-        <div class='header'>
-					<a href='/profile/${postedBy.username}' class="displayName">${displayName}</a>
-					<span class='username'>@${postedBy.username}</span>
-					<span class='date'>${timestamp}</span>
+      <div class='mainContentContainer'>
+        <div class='userImageContainer'>
+          <img src='${postedBy.profilePic}'>
         </div>
-        <div class='postBody'>
-          <span>${postData.content}</span>
+        <div class='postContentContainer'>
+          <div class='header'>
+            <a href='/profile/${postedBy.username}' class="displayName">${displayName}</a>
+            <span class='username'>@${postedBy.username}</span>
+            <span class='date'>${timestamp}</span>
+          </div>
+          <div class='postBody'>
+            <span>${postData.content}</span>
+          </div>
+          <div class='postFooter'>
+            <div class="postButtonContainer">
+              <button>
+                <i class="far fa-comment"></i>
+              </button>
+            </div>
+            <div class="postButtonContainer">
+              <button>
+                <i class="fas fa-retweet"></i>
+              </button>
+            </div>
+            <div class="postButtonContainer">
+              <button>
+                <i class="far fa-heart"></i>
+              </button>
+            </div>
+          </div>
         </div>
-        <div class='postFooter'>
-					<div class="postButtonContainer">
-						<button>
-							<i class="far fa-comment"></i>
-						</button>
-					</div>
-					<div class="postButtonContainer">
-						<button>
-							<i class="fas fa-retweet"></i>
-						</button>
-					</div>
-					<div class="postButtonContainer">
-						<button>
-							<i class="far fa-heart"></i>
-						</button>
-					</div>
-        </div>
-      </div>
-    </div>`;
-	console.log(div);
+      </div>`;
 
 	return div;
+}
+
+function timeDifference(current, previous) {
+	const msPerMinute = 60 * 1000;
+	const msPerHour = msPerMinute * 60;
+	const msPerDay = msPerHour * 24;
+	const msPerMonth = msPerDay * 30;
+	const msPerYear = msPerDay * 365;
+
+	const elapsed = current - previous;
+
+	if (elapsed < msPerMinute) {
+		if (elapsed / 1000 < 30) return "Just now";
+		return Math.round(elapsed / 1000) + " seconds ago";
+	} else if (elapsed < msPerHour) {
+		return Math.round(elapsed / msPerMinute) + " minutes ago";
+	} else if (elapsed < msPerDay) {
+		return Math.round(elapsed / msPerHour) + " hours ago";
+	} else if (elapsed < msPerMonth) {
+		return Math.round(elapsed / msPerDay) + " days ago";
+	} else if (elapsed < msPerYear) {
+		return "over " + Math.round(elapsed / msPerMonth) + " months ago";
+	} else {
+		return "over " + Math.round(elapsed / msPerYear) + " years ago";
+	}
 }
 
 // $("#postTextarea").keyup((event) => {
