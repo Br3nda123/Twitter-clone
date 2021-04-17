9488126ef099b2a9ac9ee9e151774428dc50c6f6
Brandon Kunicki <brandon.kunicki@gmail.com>
2021-04-17 20:09:03 +0200

    Create Route in profilePage to render html with good data. Add route to app.js

---
+++ routes/profileRoutes.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const app = express();
+const router = express.Router();
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
+
+const User = require("../schemas/UserSchema");
+
+router.get("/", (req, res, next) => {
+	const payload = {
+		pageTitle: req.session.user.username,
+		userLoggedIn: req.session.user,
+		userLoggedInJs: JSON.stringify(req.session.user),
+		profileUser: req.session.user,
+	};
+
+	res.status(200).render("profilePage", payload);
+});
+
+router.get("/:username", async (req, res, next) => {
+	const payload = await getPayload(req.params.username, req.session.user);
+
+	res.status(200).render("profilePage", payload);
+});
+
+router.get("/:username/replies", async (req, res, next) => {
+	let payload = await getPayload(req.params.username, req.session.user);
+	payload.selectedTab = "replies";
+
+	res.status(200).render("profilePage", payload);
+});
+
+async function getPayload(username, userLoggedIn) {
+	let user = await User.findOne({ username });
+
+	if (user == null) {
+		let ObjectId = require("mongoose").Types.ObjectId;
+		if (!ObjectId.isValid(username)) {
+			return {
+				pageTitle: "User not found",
+				userLoggedIn,
+				userLoggedInJs: JSON.stringify(userLoggedIn),
+			};
+		}
+
+		user = await User.findById(username);
+		if (user == null) {
+			return {
+				pageTitle: "User not found",
+				userLoggedIn,
+				userLoggedInJs: JSON.stringify(userLoggedIn),
+			};
+		}
+	}
+
+	return {
+		pageTitle: user.username,
+		userLoggedIn,
+		userLoggedInJs: JSON.stringify(userLoggedIn),
+		profileUser: user,
+	};
+}
+
+module.exports = router;
