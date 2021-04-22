8f07e6c8cd32760c7f591820c95d46d6ac55ccaa
Brandon Kunicki <brandon.kunicki@gmail.com>
2021-04-22 21:21:55 +0200

    Transferred 2 function from followers... to common.js

--- public/js/common.js
+++ public/js/common.js
@@ -573,3 +573,52 @@ function outputPostsWithReplies(results, container) {
 		container.appendChild(html);
 	});
 }
+
+function outputUsers(results, container) {
+	container.innerHTML = "";
+
+	results.forEach((result) => {
+		const html = createUserHtml(result, true);
+		container.appendChild(html);
+	});
+
+	if (results.length == 0) {
+		container.innerHTML = "<span class='noResults'>No results found</span>";
+	}
+}
+
+function createUserHtml(userData, showFollowButton) {
+	const { firstName, lastName, username, profilePic } = userData;
+	const name = firstName + " " + lastName;
+	const isFollowing =
+		userLoggedIn.following && userLoggedIn.following.includes(userData._id);
+	const text = isFollowing ? "Following" : "Follow";
+	const buttonClass = isFollowing ? "followButton following" : "followButton";
+
+	let followButton = "";
+	if (showFollowButton && userLoggedIn._id != userData._id) {
+		followButton = `
+    <div class='followButtonContainer'>
+      <button class='${buttonClass}' data-user='${userData._id}'>${text}</button>
+    </div>
+    `;
+	}
+
+	const div = document.createElement("div");
+	div.classList.add = "user";
+	div.innerHTML = `
+  <div class='user'>
+    <div class='userImageContainer'>
+      <img src='${profilePic}'>
+    </div>
+    <div class='userDetailsContainer'>
+      <div class='header'>
+        <a href='/profile/${username}'>${name}</a>
+        <span class='username'>@${username}</span>
+      </div>
+    </div>
+    ${followButton}
+  </div>`;
+
+	return div;
+}
