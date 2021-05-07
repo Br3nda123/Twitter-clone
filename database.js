445b3f3958dbb6935134d940d074f97366c56222
Brandon Kunicki <brandon.kunicki@gmail.com>
2021-05-07 22:36:22 +0200

    Generate project by Express application generator and fix bugs

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 mongoose.set("useNewUrlParser", true);
 mongoose.set("useUnifiedTopology", true);
-mongoose.set("useFindAndModify", false);
+// mongoose.set("useFindAndModify", false);
 
 class Database {
 	constructor() {
