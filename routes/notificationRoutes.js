e761b16cfbd21eba7976ebf3e00c832a608bf4e6
Brandon Kunicki <brandon.kunicki@gmail.com>
2021-05-07 21:46:09 +0200

    Delete usless imports

--- routes/notificationRoutes.js
+++ routes/notificationRoutes.js
@@ -1,12 +1,6 @@
 const express = require("express");
 const app = express();
 const router = express.Router();
-const bodyParser = require("body-parser");
-const bcrypt = require("bcrypt");
-const mongoose = require("mongoose");
-
-const User = require("../schemas/UserSchema");
-const Chat = require("../schemas/ChatSchema");
 
 router.get("/", (req, res, next) => {
 	const payload = createPayload("Notifications", req.session.user);
--- routes/searchRoutes.js
+++ routes/searchRoutes.js
@@ -1,10 +1,6 @@
 const express = require("express");
 const app = express();
 const router = express.Router();
-const bodyParser = require("body-parser");
-const bcrypt = require("bcrypt");
-
-const User = require("../schemas/UserSchema");
 
 router.get("/", (req, res, next) => {
 	const payload = createPayload(req.session.user);
